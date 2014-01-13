e175fcf18516a9145745d84240da69e0571eb53e
tnkjapan <yuto.yasunaga@gmail.com>
2014-01-13 12:05:24 +0700

    New project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TnkContact::Application.config.secret_key_base = 'aba5c48edcc1658439f8f95b34ff2fe07998f3b836b4275099afe4070447cf04fa0d585cb8a3e79e9cce345a32ba4598168f6728ae83bcd7d424d443d2d2b05e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TnkContact::Application.config.session_store :cookie_store, key: '_tnk-contact_session'
