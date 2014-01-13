8fa0d0b4ef7de7b981c782985834a5f47ee66f2d
tnkjapan <yuto.yasunaga@gmail.com>
2014-01-13 12:28:33 +0700

    Created controller 'home' include actions 'index' and 'done'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TnkContact::Application.routes.draw do
+  root "home#index"
+  get "/done" => "home#done"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
