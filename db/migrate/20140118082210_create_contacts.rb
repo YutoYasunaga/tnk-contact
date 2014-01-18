ace8250a9229e47738b26b5e9baa8a212c26f288
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2014-01-18 17:39:02 +0900

    setup relation between models

--- db/migrate/20140118082210_create_contacts.rb
+++ db/migrate/20140118082210_create_contacts.rb
@@ -1,11 +1,14 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-      t.string :name
-      t.string :email
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :city_ids
       t.text :description
 
       t.timestamps
     end
+
+    add_index :contacts, :email
   end
 end
