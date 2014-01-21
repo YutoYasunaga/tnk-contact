475918e1a595ae9a998fb177d78407330d7c4f81
tnkjapan <yuto.yasunaga@gmail.com>
2014-01-21 17:46:48 +0700

    Rollback and will work with mail from now

--- app/mailers/contact_mailer.rb
+++
@@ -1,6 +0,0 @@
-class ContactMailer < ActionMailer::Base
-  default from: "from@example.com"
-  def welcome
-    mail(to: "morumotto26@gmail.com", subject: "Welcome", body: "Have a nice day")
-  end
-end
