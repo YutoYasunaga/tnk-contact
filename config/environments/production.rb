67d9438de7547b785c9d6ffaf388c32e7a27a5d7
tnkjapan <yuto.yasunaga@gmail.com>
2014-01-21 17:06:19 +0700

    fix dev and produc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,9 @@
 TnkContact::Application.configure do
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: "heroku.com" }
+  
   config.action_mailer.smtp_settings = {
     :address => "smtp.mandrillapp.com",
     :port => 587,
