4ce5d55d48a0e6506ee4973e5543e0ea03e2d2a0
tnkjapan <yuto.yasunaga@gmail.com>
2014-01-21 18:27:28 +0700

    can send mail to multiple recipients

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,6 +3,6 @@ class ContactMailer < ActionMailer::Base
   def send_mail(recipient)
     @recipient = recipient
     @sender = Contact.last
-    mail(to: @recipient, subject: "#{@sender.name}さんからのメール")
+    mail(to: ["yuto.yasunaga@gmail.com", @recipient], subject: "#{@sender.name}さんからのメール")
   end
 end
