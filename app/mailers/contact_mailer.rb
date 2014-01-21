a498391100b4125650eb6e3a7c87efc9d70b1830
tnkjapan <yuto.yasunaga@gmail.com>
2014-01-21 16:48:49 +0700

    Updated production

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,4 +1,6 @@
 class ContactMailer < ActionMailer::Base
+  ActionMailer::Base.smtp_settings = {}
+  ActionMailer::Base.delivery_method = :smtp
   default from: "from@example.com"
   def welcome
     mail(to: "morumotto26@gmail.com", subject: "Welcome", body: "Have a nice day")
