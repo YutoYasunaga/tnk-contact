cebab47682296792ac96cb9267454602ec4eb7ce
tnkjapan <yuto.yasunaga@gmail.com>
2014-01-22 11:29:52 +0700

    fixed contactmailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ActionMailer::Base
   def send_mail(recipient)
     @recipient = recipient
     @sender = Contact.last
-    mail(from: "#{@sender.name}", to: "#{@recipient}", subject: "#{@sender.email}").deliver
+    mail(from: @sender.name, to: @recipient, subject: @sender.email).deliver
   end
 end
