d0493a5ebefde18848a0f618dabd658b81292af9
tnkjapan <yuto.yasunaga@gmail.com>
2014-01-22 12:11:02 +0700

    mail default from

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,8 @@
 class ContactMailer < ActionMailer::Base
+  default from: "Admin@example.com"
   def send_mail(recipient)
     @recipient = recipient
     @sender = Contact.last
-    mail(from: "Admin", to: @recipient, subject: "Test mail")
+    mail(to: @recipient, subject: "Test mail")
   end
 end
