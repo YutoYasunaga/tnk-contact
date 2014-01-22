8fac9e42c28e8b4fcc05278e8204e82c4c81830c
tnkjapan <yuto.yasunaga@gmail.com>
2014-01-22 13:40:34 +0700

    test branch mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,11 +2,13 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     @contact.save
+    @text = String.new
     @contact.city_ids.each do |c|
       @city = City.where("id = ?", c).take
       @branch = Branch.where("id = ?", @city.branch_id).take
+      @text.concat(@branch.email)
     end
-    ContactMailer.send_mail("morumotto26@gmail.com").deliver
+    ContactMailer.send_mail("morumotto26@gmail.com", @text).deliver
     redirect_to controller: "home", action: "done"
   end
 
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,9 @@
 class ContactMailer < ActionMailer::Base
   default from: "auto@tnkjapan.com"
-  def send_mail(recipient)
+  def send_mail(recipient, content)
+    @content = content
     @recipient = recipient
     @sender = Contact.last
-    mail(to: @recipient, subject: "#{@sender.name}")
+    mail(to: @recipient, subject: "#{@sender.name}", body: @content)
   end
 end
