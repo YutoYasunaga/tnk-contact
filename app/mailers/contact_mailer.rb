031d609cf363722b47a662d63a6c9dddd0deff0b
tnkjapan <yuto.yasunaga@gmail.com>
2014-01-22 11:35:08 +0700

    Fixed test mail

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ActionMailer::Base
   def send_mail(recipient)
     @recipient = recipient
     @sender = Contact.last
-    mail(from: @sender.name, to: @recipient, subject: @sender.email).deliver
+    mail(from: "Admin", to: @recipient, subject: "Test mail").deliver
   end
 end
